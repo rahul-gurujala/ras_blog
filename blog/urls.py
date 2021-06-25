afdb4ef585e38bb69aecd3adc5b8ad56d7b26ec1
rahul-gurujala <rahul.gurujala@neosoftmail.com>
2021-06-26 00:20:00 +0530

    Completed Blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('posts/', views.posts, name='post'),
-    path('posts/<slug:slug>', views.post_details, name='post-details'),
+    path('posts/<slug:slug>', views.post_details, name='post-detail'),
 ]
